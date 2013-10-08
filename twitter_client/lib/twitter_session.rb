d0d6193d087fc81fb897e454c626cbc9448c116b
App Academy Student <student@appacademy.io>
2013-10-08 14:56:43 -0700

    Initial commit twitter_client 'clamour'

---
+++ twitter_client/lib/twitter_session.rb
@@ -0,0 +1,15 @@
+class TwitterSession
+  include Singleton
+
+  CONSUMER_KEY = "eMeovi2qKjxmRKOZGYUwQ"
+  CONSUMER_SECRET = "8I6r7cUNqbtzfrHxF93A6hkMG9YYQRnF4Qp7jv9doE"
+
+  def self.get(*args)
+
+  end
+
+  def self.post(*args)
+
+  end
+
+end
