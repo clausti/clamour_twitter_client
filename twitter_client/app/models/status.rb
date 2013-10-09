2a5ce3506158fee5c585500ce5545d9b064a1f56
App Academy Student <student@appacademy.io>
2013-10-08 17:50:44 -0700

    Implements Status fetching

--- twitter_client/app/models/status.rb
+++ twitter_client/app/models/status.rb
@@ -1,3 +1,54 @@
 class Status < ActiveRecord::Base
   # attr_accessible :title, :body
+  attr_accessible :twitter_user_id, :twitter_status_id, :body
+
+  validates :twitter_status_id, :uniqueness => true, :presence => true
+  validates :twitter_user_id, :presence => true
+  validates :body, :presence => true
+
+
+  def self.fetch_statuses_for_user(twitter_user_id)
+    get_statuses_url = Addressable::URI.new(
+                         :scheme => "https",
+                         :host => "api.twitter.com",
+                         :path => "/1.1/statuses/user_timeline.json",
+                         :query_values => { :user_id => twitter_user_id }
+                         ).to_s
+
+    json_statuses = TwitterSession.get(get_statuses_url) #array or something
+    status_objects = []
+
+    json_statuses.each do |status|
+      status_objects << self.retrieve_or_create(status)
+    end
+
+    status_objects
+  end
+
+
+  def self.retrieve_or_create(json_status)
+    status = Status.find_by_twitter_status_id(json_status["id_str"])
+
+    if status
+      status
+    else
+      status = self.parse_twitter_status(json_status)
+    end
+  end
+
+  def self.parse_twitter_status(json_status) #received from where?
+    twitter_status_id = json_status["id_str"]
+    twitter_user_id = json_status["user"]["id_str"]
+    body = json_status["text"]
+    Status.create({:twitter_user_id => twitter_user_id,
+                  :twitter_status_id => twitter_status_id,
+                  :body => body})
+  end
+
+  belongs_to(:user,
+             :foreign_key => :twitter_user_id,
+             :primary_key => :twitter_user_id,
+             :class_name => "User")
+
+
 end
--- twitter_client/app/models/user.rb
+++ twitter_client/app/models/user.rb
@@ -1,3 +1,41 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :twitter_user_id, :screen_name
+
+  validates :twitter_user_id, :uniqueness => true, :presence => true
+  validates :screen_name, :uniqueness => true, , :presence => true
+
+  def self.fetch_by_screen_name(screen_name)
+    get_user_url = Addressable::URI.new(
+                     :scheme => "https",
+                     :host => "api.twitter.com",
+                     :path => "/1.1/users/show.json",
+                     :query_values => { :screen_name => screen_name } ).to_s
+
+    user_as_json = TwitterSession.get(get_user_url)
+    self.parse_twitter_params(user_as_json)
+  end
+
+  def self.parse_twitter_params(user_as_json)
+    screen_name = user_as_json["screen_name"]
+    twitter_user_id = user_as_json["id_str"]
+
+    User.create({:screen_name => screen_name, :twitter_user_id => twitter_user_id})
+  end
+
+  has_many(:statuses,
+           :foreign_key => :twitter_user_id,
+           :primary_key => :twitter_user_id,
+           :class_name => "Status")
+
+  def sync_statuses
+    statuses = Status.fetch_statuses_for_user(@twitter_user_id)
+    statuses.each do |status|
+      if status.persisted?
+        next
+      else
+        status.save!
+      end
+    end
+  end
+
 end
